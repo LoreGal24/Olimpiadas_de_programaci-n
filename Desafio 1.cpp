c4bd5e9ed589fbaaf534996036d233209f084a6d
LoreGal24 <163057427+LoreGal24@users.noreply.github.com>
2024-07-28 17:28:35 -0300

    Desafio 1 completado

---
+++ Desafio 1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int zetadieta(int C, int P, int G){
+    int resultado;
+    double cantidadC=C/27;
+    double cantidadP=P/30;
+    int cantidadG=G;
+    
+    if(C%27 != 0){
+        cantidadC++;
+        // estoy agregando +1 si es un decimal (osea redondear para arriba)
+    }
+    
+    if(P%30 != 0){
+        cantidadP++;
+        // estoy agregando +1 si es un decimal (osea redondear para arriba)
+    }
+    
+    resultado= cantidadC*105 + cantidadP*120 + cantidadG*9; //calcula la cantidad de calorias que se consume
+    
+    return resultado;
+}
+
+int main(){
+    
+    cout <<"la cantida de calorias que consumiras sera: " <<  zetadieta(88, 90, 50) << endl;
+    cout <<"en la subtarea 1 la cantida de calorias que consumiras sera: " <<  zetadieta(0, 0, 999) << endl;
+    cout <<"en la subtarea 2 la cantida de calorias que consumiras sera: " <<  zetadieta(0, 0, 3000) << endl;
+    cout <<"en la subtarea 3 la cantida de calorias que consumiras sera: " <<  zetadieta(0, 999, 0) << endl;
+    cout <<"en la subtarea 4 la cantida de calorias que consumiras sera: " <<  zetadieta(0, 3000, 0) << endl;
+    cout <<"en la subtarea 5 la cantida de calorias que consumiras sera: " <<  zetadieta(999, 0, 0) << endl;
+    cout <<"en la subtarea 6 la cantida de calorias que consumiras sera: " <<  zetadieta(3000, 0, 0) << endl;
+    cout <<"en la subtarea 7 la cantida de calorias que consumiras sera: " <<  zetadieta(9, 99, 999) << endl;
+    cout <<"en la subtarea 8 la cantida de calorias que consumiras sera: " <<  zetadieta(1001, 2000, 3000) << endl;
+    //aca testeo las subtareas en mi funcion zetadieta
+    
+    
+   
+}
