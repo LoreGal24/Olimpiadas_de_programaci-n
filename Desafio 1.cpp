6cf4a3e009a187c67024a0c50e98ac2786575b3c
LoreGal24 <163057427+LoreGal24@users.noreply.github.com>
2024-07-28 18:27:02 -0300

    Corregido

--- Desafio 1.cpp
+++ Desafio 1.cpp
@@ -6,18 +6,31 @@ int zetadieta(int C, int P, int G){
     double cantidadC=C/27;
     double cantidadP=P/30;
     int cantidadG=G;
+
+    if(C >= 0 && P >= 0 && G >= 0 ){ //esto verifica que los valores sean menores o iguales a 0
+        if((C && P && G) < 1000*1000*1000){ //esto verifica que sea menor que 10**9
+                if(C%27 != 0){
+                     cantidadC++;
+                    // estoy agregando +1 si es un decimal (osea redondear para arriba)
+                 }
     
-    if(C%27 != 0){
-        cantidadC++;
-        // estoy agregando +1 si es un decimal (osea redondear para arriba)
+                 if(P%30 != 0){
+                    cantidadP++;
+                    // estoy agregando +1 si es un decimal (osea redondear para arriba)
+                 }
+ 
+                resultado= cantidadC*105 + cantidadP*120 + cantidadG*9; //calcula la cantidad de calorias que se consume
+    
+        }
     }
     
-    if(P%30 != 0){
-        cantidadP++;
-        // estoy agregando +1 si es un decimal (osea redondear para arriba)
-    }
+    else{
+        resultado = 0;
+        cout << "0 porque tenes que poner un numero positivo y menor a 1000000000";
+        }
     
-    resultado= cantidadC*105 + cantidadP*120 + cantidadG*9; //calcula la cantidad de calorias que se consume
+    
+    
     
     return resultado;
 }
