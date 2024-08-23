90f6e325a6e1a29317d54839bff2e8dc2c75f29d
LoreGal24 <163057427+LoreGal24@users.noreply.github.com>
2024-08-23 00:26:06 -0300

    Agregando faltantes
    Agragando los contadores de tiempo, de retrocesos y de los caminos incorrectos a medias, ademas agrege la impresion del mapa en la consola

--- Desafío 4.cpp
+++ Desafío 4.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
 
 using namespace std;
 
+int retrocedio=0;
+bool retrocediendo;
+int caminosError=0;
+
 void printResult(bool resultado) {
     if(resultado){
-        cout << "existe un camino para llegar a la meta";
+        cout << "existe un camino para llegar a la meta que es:"<<endl;
     }else{
-        cout << "no hay forma de llegar a la meta";
+        cout << "no hay forma de llegar a la meta "<<endl;
     }
+    
 }
 
-bool findPath(vector<vector<char>> values, int x, int y) {
+void printResultMapa(bool resultado, vector<vector<char>> mapaActualizado) {
+    if(resultado){
+        for(int x=0; x<mapaActualizado.size()-1;x++){
+        cout << "{ ";
+        for(int y=0; y<mapaActualizado[x].size();y++){
+            cout <<mapaActualizado[x][y]<<", ";
+        }
+        cout << " }"<<endl;
+       
+    }
+    }
+    
+    
+}
+
+bool findPath(vector<vector<char>>& values, int x, int y) {
     
     if (x < 0 || y < 0 || x >= values.size() || y >= values[0].size()) {
         return false;
@@ -31,36 +52,82 @@ bool findPath(vector<vector<char>> values, int x, int y) {
     
     if(findPath(values, x, y + 1)){
         return true;
+        if(retrocediendo){
+            caminosError++;
+            retrocediendo=false;
+        }
     }
     
     if(findPath(values, x + 1, y)){
         return true;
+        if(retrocediendo){
+            caminosError++;
+            retrocediendo=false;
+        }
     }
     
     if(findPath(values, x, y - 1)){
         return true;
+        if(retrocediendo){
+            caminosError++;
+            retrocediendo=false;
+        }
     }
     
     if(findPath(values, x - 1, y)){
         return true;
+        if(retrocediendo){
+            caminosError++;
+            retrocediendo=false;
+        }
     }
     
+   
+    retrocediendo=true;
     values[x][y] = '.';
+    retrocedio++;
     return false;
 }
 
 int main() {
+    clock_t tiempo;
+    bool confirmador;
     
     vector<vector<char>> mapa = {
         {'.', '#', '#', '#', '#', '#'},
         {'.', '.', '.', '.', '.', '#'},
         {'#', '.', '#', '.', '#', '#'},
         {'#', '.', '#', '.', '#', '#'},
+        {'#', '.', '#', '.', '.', '.'},
+        {'#', '.', '#', '.', '#', '#'},
+        {'.', '#', '#', '.', '.', '#'},
+        {'.', '.', '.', '.', '.', '#'},
+        {'#', '.', '#', '.', '#', '#'},
+        {'#', '.', '#', '.', '#', '#'},
+        {'#', '.', '#', '.', '.', '.'},
+        {'#', '.', '#', '.', '#', '.'},
+        {'.', '#', '#', '#', '#', '.'},
+        {'.', '.', '.', '.', '.', '.'},
+        {'#', '.', '#', '.', '#', '#'},
+        {'#', '.', '#', '.', '#', '#'},
+        {'#', '.', '#', '.', '.', '.'},
+        {'#', '.', '#', '.', '.', '#'},
+        {'.', '#', '#', '#', '.', '#'},
+        {'.', '.', '.', '.', '.', '#'},
+        {'#', '.', '#', '.', '#', '#'},
+        {'#', '.', '#', '.', '#', '#'},
         {'#', '.', '#', '.', '.', 'G'},
         {'#', '.', '#', '.', '#', '#'}
     };
-
-    printResult(findPath(mapa, 0, 0));
-
+    tiempo=clock();
+    confirmador=findPath(mapa, 0, 0);
+    printResult(confirmador);
+    printResultMapa(confirmador, mapa);
+    tiempo=clock()-tiempo;
+    
+    cout << "el timpo que tardo en encontrar el camino correcto fue: "<< (double(tiempo)/CLOCKS_PER_SEC)*1000<<" milisegundos" << endl;
+    cout << "la cantidad de veces que tuvo que retroceder fue: "<<retrocedio<< " veces" << endl;
+    cout << "la cantidad e caminos incorrectos que encontro fue de: "<< caminosError;
+   // cout << "la cantidad de veces que entro: "<< vecesRetroceso(retrocedio) << endl;
     return 0;
 }
